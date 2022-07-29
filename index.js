cce303589ad12c5233df149992c2fc3e4315e51c
Matt Groff <matt@umbragestudios.com>
2022-07-29 12:12:13 -0400

    Cleanup and add hono to deps

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import { Hono } from 'hono'
-import { graphqlServer } from '@honojs/graphql-server'
+import { Hono } from 'hono';
+import { graphqlServer } from '@honojs/graphql-server';
 import { buildSchema } from 'graphql/utilities/buildASTSchema.js';
 
-export const app = new Hono()
+export const app = new Hono();
 
 const schema = buildSchema(`
 type Query {
   hello: String
 }
-`)
+`);
 
 const rootValue = {
   hello: () => 'Hello Hono!',
-}
+};
 
 
 app.use(
@@ -21,9 +21,9 @@ app.use(
     schema,
     rootValue,
   })
-)
+);
 
 export default {
   port: process.env.PORT || 3000,
   fetch: app.fetch,
-}
+};
